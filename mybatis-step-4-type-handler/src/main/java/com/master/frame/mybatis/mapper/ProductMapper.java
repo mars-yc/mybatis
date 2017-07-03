ef280249e0ad15d13ce2a24f8d75dfae7ec304f3
dearmaster windows7 <694114533@qq.com>
2017-07-03 23:38:32 +0800

    package scan for mapper and typeAlias finish

--- mybatis-step-4-type-handler/src/main/java/com/master/frame/mybatis/mapper/ProductMapper.java
+++ mybatis-step-4-type-handler/src/main/java/com/master/frame/mybatis/mapper/ProductMapper.java
@@ -1,14 +1,30 @@
 package com.master.frame.mybatis.mapper;
 
 import com.master.frame.mybatis.domain.Product;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
+/**
+ * When use the package scan for mappers, need to annotate the mapper class with sql
+ * <br>
+ * and remove the *Mapper.xml
+ */
 public interface ProductMapper {
 
+    @Select(value = "select * from mybatis_product_tbl")
     List<Product> load();
+    @Insert(value = "insert into mybatis_product_tbl(category,buyDate) values(#{category},#{buyDate})")
     int add(Product product);
+    @Update(value = "create table mybatis_product_tbl (\n" +
+            "        id int not null auto_increment,\n" +
+            "        category varchar(20) not null,\n" +
+            "        buyDate datetime,\n" +
+            "        primary key (id))")
     void createProductTbl();
+    @Update(value = "drop table if exists mybatis_product_tbl")
     void dropIfExistProductTbl();
 
 }
